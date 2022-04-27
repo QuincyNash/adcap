7e881fc7b743972bdc0e465df7b570c88ee6f56b
quincynash <quincy.nash@icloud.com>
2022-04-26 18:27:10 -0700

    Remove next-env.d.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from "next/head";
 import clientPromise from "../lib/mongodb";
 
-export default function Home(props: { isConnected }) {
+export default function Home(props: {}) {
 	return <div></div>;
 }
 
 export async function getServerSideProps(context) {
-	try {
-		const db = await clientPromise;
-	} catch (e) {
-		console.error(e);
-	}
+	const db = await clientPromise;
+	return {
+		props: {},
+	};
 }
